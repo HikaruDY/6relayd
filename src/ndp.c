HikaruDY/6relayd

commit 61bcbeb53db3473531a09d56e770f8b9edd7261f

Register route: Use 'ip' command instead of RTNETLINK

src/ndp.c
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
-
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <net/ethernet.h>
@@ -192,11 +192,33 @@ int init_ndp_proxy(const struct relayd_config *relayd_config)
 // Deinitialize NDP proxy
 void deinit_ndp_proxy()
 {
+	char Proto[4] = {0};
+	char *IPCommandArgs[9] = {0};
+	IPCommandArgs[0] = "ip";
+	IPCommandArgs[1] = "-6";
+	IPCommandArgs[2] = "route";
+	IPCommandArgs[3] = "flush";
+	IPCommandArgs[4] = "proto";
+	IPCommandArgs[5] = Proto;
+	strcpy(Proto, config->route_proto);
+
+	printf("I: Unregistering routes...\n");
+//	printf("I: DEBUG: %s %s %s %s %s %s\n", IPCommandArgs[0], IPCommandArgs[1], IPCommandArgs[2], IPCommandArgs[3], IPCommandArgs[4], IPCommandArgs[5]);
+
+	pid_t P = fork();
+	if(P == 0){
+		execvp("ip", IPCommandArgs);
+		printf("E: Failed to exec: %i\n", errno);
+		exit(0);
+	}
+
+/*
 	while (!list_empty(&neighbors)) {
 		struct ndp_neighbor *c = list_first_entry(&neighbors,
 				struct ndp_neighbor, head);
 		modify_neighbor(&c->addr, c->iface, false);
 	}
+*/
 }
 
 
@@ -353,6 +375,12 @@ void relayd_setup_route(const struct in6_addr *addr, int prefixlen,
 }
 
 // Use rtnetlink to modify kernel routes
+// Use 'ip' command to modify kernel routes (rtnetlink method is not working...)
+
+static char *IPCommandArgs[9] = {0};
+static char IPv6[64];
+static char Proto[4];
+
 static void setup_route(struct in6_addr *addr, struct relayd_interface *iface,
 		bool add)
 {
@@ -361,10 +389,40 @@ static void setup_route(struct in6_addr *addr, struct relayd_interface *iface,
 	syslog(LOG_NOTICE, "%s about %s on %s", (add) ? "Learned" : "Forgot",
 			namebuf, (iface) ? iface->ifname : "<pending>");
 
-	if (!iface || !config->enable_route_learning)
+	if (!iface || !config->enable_route_learning){
 		return;
+	}
 
-	relayd_setup_route(addr, 128, iface, NULL, add);
+	if(IPCommandArgs[0] == 0){
+		IPCommandArgs[0] = "ip";
+		IPCommandArgs[1] = "-6";
+		IPCommandArgs[2] = "route";
+		IPCommandArgs[5] = "dev";
+		IPCommandArgs[7] = "proto";
+		IPCommandArgs[8] = Proto;
+	}
+	if(add){
+		IPCommandArgs[3] = "add";
+	} else {
+		IPCommandArgs[3] = "del";
+	}
+
+	strcpy(IPv6, namebuf);
+	strcpy(Proto, config->route_proto);
+
+	IPCommandArgs[4] = IPv6;
+	IPCommandArgs[6] = iface->ifname;
+	printf("I: %s Route: %s -> %s\n", IPCommandArgs[3], IPCommandArgs[4], IPCommandArgs[6]);
+//	printf("I: DEBUG: %s %s %s %s %s %s %s %s %s\n", IPCommandArgs[0], IPCommandArgs[1], IPCommandArgs[2], IPCommandArgs[3], IPCommandArgs[4], IPCommandArgs[5], IPCommandArgs[6], IPCommandArgs[7], IPCommandArgs[8]);
+
+	pid_t P = fork();
+	if(P == 0){
+		execvp("ip", IPCommandArgs);
+		printf("E: Failed to exec: %i\n", errno);
+		exit(0);
+	}
+
+//	relayd_setup_route(addr, 128, iface, NULL, add);
 }
 
 static void free_neighbor(struct ndp_neighbor *n)

src/6relayd.c
@@ -57,16 +57,18 @@ static int open_interface(struct relayd_interface *iface,
 		const char *ifname, bool external);
 static void relayd_receive_packets(struct relayd_event *event);
 
-
 int main(int argc, char* const argv[])
 {
 	memset(&config, 0, sizeof(config));
+
+	sprintf(config.route_proto, "66");
+	int RP = 0;
 
 	const char *pidfile = "/var/run/6relayd.pid";
 	bool daemonize = false;
 	int verbosity = 0;
 	int c;
-	while ((c = getopt(argc, argv, "ASR:D:Nsucn::l:a:rt:m:oi:p:dvh")) != -1) {
+	while ((c = getopt(argc, argv, "ASR:D:Nsucn::l:a:rt:m:oi:p:dvh:P:")) != -1) {
 		switch (c) {
 		case 'A':
 			config.enable_router_discovery_relay = true;
@@ -168,6 +170,14 @@ int main(int argc, char* const argv[])
 
 		case 'v':
 			verbosity++;
+			break;
+
+		case 'P':
+			RP = atoi(optarg);
+			if((RP) && (RP < 256)){
+				sprintf(config.route_proto, optarg);
+			}
+			printf("I: Route proto set to %s\n", config.route_proto);
 			break;
 
 		default:

src/6relayd.h
@@ -107,6 +107,8 @@ struct relayd_config {
 
 	char** static_ndp;
 	size_t static_ndp_len;
+
+	char route_proto[4];
 };
 
 

src/dhcpv6-ia.c
@@ -17,6 +17,7 @@
 #include "dhcpv6.h"
 #include "md5.h"
 
+#include <stdio.h>
 #include <time.h>
 #include <errno.h>
 #include <fcntl.h>

commit 09ea70bafb67ef672ee85abde7d2a20757349bb7

Fix adding routes (Fix "E: Failed to exec: 14")
The problem manifested with a route not added and log messages such as:

    I: add Route: 2001:db8:xxxx -> eth1
    E: Failed to exec: 14

errno code 14 is EFAULT. The child process was crashing at
    execvp("ip", IPCommandArgs);
since IPCommandArgs list was not NULL-pointer terminated.

Fixes: 61bcbeb5 ("Register route: Use 'ip' command instead of RTNETLINK")

Signed-off-by: Andrej Furlan <andrej@plume.com>

src/ndp.c
@@ -377,7 +377,7 @@ void relayd_setup_route(const struct in6_addr *addr, int prefixlen,
 // Use rtnetlink to modify kernel routes
 // Use 'ip' command to modify kernel routes (rtnetlink method is not working...)
 
-static char *IPCommandArgs[9] = {0};
+static char *IPCommandArgs[10] = {0};
 static char IPv6[64];
 static char Proto[4];
 